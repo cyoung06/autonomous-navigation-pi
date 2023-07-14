e3f8d20ff7d48be6f0773250edab98d8bf81375d
syeyoung <cyoung06@naver.com>
2023-07-14 17:44:39 +0900

    init

---
+++ arduino_serial.py
@@ -0,0 +1,38 @@
+import math
+import threading
+
+import serial
+from threading import Thread
+
+
+class MovingPlatform:
+    def __init__(self, port):
+        self.port = serial.Serial(port)
+        self.waitFor("Cnc shield init!")
+        self.thread = threading.Thread(target=self.readLines)
+        self.thread.start()
+        self.lastLine = b"DONE"
+
+    def readLines(self):
+        while True:
+            self.lastLine = self.port.readline()
+            print(f"received... {self.lastLine}")
+
+    # y is forward x is sideways
+    def goVector(self, vec, dist):
+        if not self.isDone():
+            raise "Not done"
+        self.lastLine = ""
+        self.port.write(f"{vec[0]} {vec[1]} {dist} 0 0 ")
+
+    def goForward(self, dist):
+        self.goVector([0, 1], dist)
+
+    def rotateCW(self, deg):
+        if not self.isDone():
+            raise "Not done"
+        self.lastLine = ""
+        self.port.write(f'0 0 0 0 {"{:.8f}".format(deg * math.pi / 180.0)}')
+
+    def isDone(self):
+        return self.lastLine == b"MOVED"
---
+++ main.py
@@ -0,0 +1,30 @@
+# MPU6050 9-DoF Example Printout
+import sys
+from os.path import dirname
+from arduino_serial import MovingPlatform
+
+sys.path.append(dirname(__file__))
+print(sys.path)
+
+from mpu9250_i2c import *
+
+if __name__ == "__main__":
+    platform = MovingPlatform(sys.argv[1])
+    time.sleep(1)  # delay necessary to allow mpu9250 to settle
+
+
+    print('recording data')
+    while 1:
+        if platform.isDone():
+            platform.goForward(10)
+        try:
+            ax, ay, az, wx, wy, wz = mpu6050_conv()  # read and convert mpu6050 data
+            mx, my, mz = AK8963_conv()  # read and convert AK8963 magnetometer data
+        except:
+            continue
+
+        print('{}'.format('-' * 30))
+        print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax, ay, az))
+        print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx, wy, wz))
+        print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx, my, mz))
+        print('{}'.format('-' * 30))
