1d1df9717ae9f2854f2e6e873f1741259d31b0ef
syeyoung <cyoung06@naver.com>
2023-07-14 17:56:21 +0900

    are ya ready?

--- arduino_serial.py
+++ arduino_serial.py
@@ -16,20 +16,24 @@ class MovingPlatform:
 
         self.thread = threading.Thread(target=self.readLines)
         self.thread.start()
-        self.lastLine = b"MOVED\r\n"
+        self.ready = True
 
         time.sleep(2.0)
 
     def readLines(self):
         while True:
-            self.lastLine = self.port.readline()
+            lastLine = self.port.readline()
             print(f"received... {self.lastLine}")
+            if lastLine == b'Cnc shield init!\r\n':
+                self.ready = True
+            if lastLine == b'MOVED\r\n':
+                self.ready = True
 
     # y is forward x is sideways
     def goVector(self, vec, dist):
         if not self.isDone():
             raise "Not done"
-        self.lastLine = ""
+        self.ready = False
         self.port.write(f"{vec[0]} {vec[1]} {dist} 0 0 ".encode())
         print("Sending... " + f"{vec[0]} {vec[1]} {dist} 0 0 ")
         self.port.flush()
@@ -40,10 +44,10 @@ class MovingPlatform:
     def rotateCW(self, deg):
         if not self.isDone():
             raise "Not done"
-        self.lastLine = ""
+        self.ready = False
         self.port.write(f'0 0 0 0 {"{:.8f}".format(deg * math.pi / 180.0)}'.encode())
         print("Sending... " + f'0 0 0 0 {"{:.8f}".format(deg * math.pi / 180.0)}')
         self.port.flush()
 
     def isDone(self):
-        return self.lastLine == b"MOVED\r\n"
+        return self.ready
