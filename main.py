e2c62aafcaf672604256b89df3267df902e03ae9
syeyoung <cyoung06@naver.com>
2023-07-14 18:46:03 +0900

    log

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # MPU6050 9-DoF Example Printout
+import random
 import sys
 from os.path import dirname
 from arduino_serial import MovingPlatform
@@ -12,20 +13,20 @@ if __name__ == "__main__":
     platform = MovingPlatform(sys.argv[1])
     time.sleep(1)  # delay necessary to allow mpu9250 to settle
 
+    with open("log.csv", "a") as log:
+        print('recording data')
+        while 1:
+            random.randint(10)
+            if platform.isDone():
+                if random.random() > 0.5:
+                    platform.goForward(random.randint(1, 10) * 10)
+                else:
+                    platform.rotateCW(2 * random.random() - 1)
+            try:
+                ax, ay, az, wx, wy, wz = mpu6050_conv()  # read and convert mpu6050 data
+                mx, my, mz = AK8963_conv()  # read and convert AK8963 magnetometer data
+            except:
+                continue
 
-    print('recording data')
-    while 1:
-        if platform.isDone():
-            platform.goForward(10)
-        try:
-            ax, ay, az, wx, wy, wz = mpu6050_conv()  # read and convert mpu6050 data
-            mx, my, mz = AK8963_conv()  # read and convert AK8963 magnetometer data
-        except:
-            continue
-
-        print('{}'.format('-' * 30))
-        print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax, ay, az))
-        print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx, wy, wz))
-        print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx, my, mz))
-        print('{}'.format('-' * 30))
-        time.sleep(0.03)
+            log.write('{0},{1},{2},{3},{4},{5},{6},{7},{8}\n'.format(ax,ay,az,wx,wy,wz,mx,my,mz))
+            time.sleep(0.03)
