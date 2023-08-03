d75cc7b01452215ef3a4a955633aa8c93815706b
syeyoung <cyoung06@naver.com>
2023-08-03 20:07:17 +0900

    it owrks?

--- sensors_i2c.py
+++ sensors_i2c.py
@@ -8,11 +8,11 @@ class Sensors:
     def readUltra(self):
         msg = i2c_msg.read(self.id2, 6)
         self.bus.i2c_rdwr(msg)
-
-        print(msg)
-        front = msg.buf[0] * 255 + msg.buf[1]
-        right = msg.buf[2] * 255 + msg.buf[3]
-        left = msg.buf[4] * 255 + msg.buf[5]
+        data = list(msg)
+        print(data)
+        front = data[0] * 255 + data[1]
+        right = data[2] * 255 + data[3]
+        left = data[4] * 255 + data[5]
         print(front)
         print(left)
         print(right)
