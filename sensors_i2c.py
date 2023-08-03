addb33893b964fc55ed6cc3ce99109d33effd679
syeyoung <cyoung06@naver.com>
2023-08-03 19:46:32 +0900

    actually send msg

--- sensors_i2c.py
+++ sensors_i2c.py
@@ -7,6 +7,7 @@ class Sensors:
 
     def readUltra(self):
         msg = i2c_msg.read(self.id2, 6)
+        self.bus.i2c_rdwr(msg)
 
         front = msg.buf[0] * 255 + msg.buf[1]
         right = msg.buf[2] * 255 + msg.buf[3]
