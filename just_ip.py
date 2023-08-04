e0471a732ba2fe60a7a7a25d674095c358fb1ae6
syeyoung <cyoung06@naver.com>
2023-08-04 16:28:54 +0900

    ip displayer

---
+++ just_ip.py
@@ -0,0 +1,15 @@
+import time
+
+import smbus2
+from lcd import LiquidCrystal
+import ip_utils
+
+bus = smbus2.SMBus(1)
+lcd = LiquidCrystal(bus)
+
+lcd.backlight(1)
+
+while True:
+    lcd.lcd_display_string(ip_utils.wifiname(), 1, 0)
+    lcd.lcd_display_string(ip_utils.get_ip(), 2, 0)
+    time.sleep(1)
