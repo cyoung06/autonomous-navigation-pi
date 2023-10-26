eba8ad7ffe5aaae5eaba4b84fb065fd5c2b43be4
syeyoung <cyoung06@naver.com>
2023-10-26 17:11:07 +0900

    Bruh

--- interactive/statusgui.py
+++ interactive/statusgui.py
@@ -22,11 +22,11 @@ class StatusGUI:
         pygame.init()
 
         display = (800, 600)
-        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        pygame.display.set_mode(display)
 
+        screen = pygame.display.set_mode([800, 600], DOUBLEBUF | OPENGL)
         gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
         # Set up the drawing window
-        screen = pygame.display.set_mode([800, 600])
 
         # Run until the user asks to quit
         running = True
