ffbbdc94f292e1049f4d3ed142c2bc8aa73997ab
syeyoung <cyoung06@naver.com>
2023-10-30 17:47:46 +0900

    line rot

--- manual3.py
+++ manual3.py
@@ -33,6 +33,6 @@ def doStuff(dx, dy, w, dist):
 
 while True:
     # a = input()
-    doStuff(0, 500, 30, 1)
-    doStuff(-250, 250 * math.sqrt(3), -60, 1)
-    doStuff(250, 250 * math.sqrt(3), 30, 1)
+    doStuff(0, 500, 120, 0.25)
+    doStuff(-250, 250 * math.sqrt(3), -240, 0.25)
+    doStuff(250, 250 * math.sqrt(3), 120, 0.25)
