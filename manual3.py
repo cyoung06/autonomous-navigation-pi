4d039ab3248ad35cc09793a0c641768936a0d1cd
syeyoung <cyoung06@naver.com>
2023-10-30 15:34:17 +0900

    line rot

--- manual3.py
+++ manual3.py
@@ -19,13 +19,13 @@ while True:
 
     # vx * cos theta
     theta = times * w * math.pi / 180
-    vx = numpy.cos(-theta) * -dx + numpy.sin(-theta) * dy
-    vy = -numpy.sin(-theta) * -dx + numpy.cos(-theta) * dy
+    vx = numpy.cos(-theta) * dx + numpy.sin(-theta) * dy
+    vy = -numpy.sin(-theta) * dx + numpy.cos(-theta) * dy
 
     rad = w * math.pi / 180
     step = 0.01
     numpy.array([vx, vy])
 
     for i in range(len(vx)):
-        serial.sendCommand(f'G {vx[i]} {vy[i]} {rad} {step}')
+        serial.sendCommand(f'G {-vx[i]} {vy[i]} {-rad} {step}')
         time.sleep(0.1)
