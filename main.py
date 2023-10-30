5ee0e5163f33b487b609ccc50d79647e81331356
syeyoung <cyoung06@naver.com>
2023-10-30 20:25:45 +0900

    line rot

--- main.py
+++ main.py
@@ -465,8 +465,8 @@ if __name__ == '__main__':
             maxY = math.ceil(pos[1] / 1000)
             maxX = math.ceil(pos[0] / 1000)
             x, y = (pos[0]/1000, pos[1]/1000)
-            print(f'{minX} {minY} {maxX} {maxY}')
-            if minY < 0 or minX < 0 or maxY >= 3 or maxX >= 3:
+            print(f'{minX} {minY} {maxX} {maxY} {minX < 0} {minY < 0} {maxX >= 3} {maxY >= 3}')
+            if minX < 0 or minY < 0 or maxX >= 3 or maxY >= 3:
                 return np.zeros(maxMacAddrs + 1)
             if access[minY][minX] is None or access[minY][maxX] is None or access[maxY][minX] is None or access[maxY][maxX] is None:
                 return np.zeros(maxMacAddrs + 1)
